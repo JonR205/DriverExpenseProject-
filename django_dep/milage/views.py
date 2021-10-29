e43eccf36c024e8ecfdeedf012fc92dd31ac0e8a
JonR205 <gohan205@gmail.com>
2021-10-28 22:07:56 -0500

    added dummy list for home page

--- django_dep/milage/views.py
+++ django_dep/milage/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'driver': 'JonathanR',
+        'title' : 'Miles Driven',
+        'miles': '136',
+        'date_driven': 'October 27 2021'
+    },
+    {
+        'driver': 'JonathanR',
+        'title' : 'Miles Driven',
+        'miles': '143',
+        'date_driven': 'October 28 2021'
+    }
+]
+
 def home(request):
-    return render(request,'milage/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request,'milage/home.html', context)
 
 def about(request):
     return render(request,'milage/about.html')
