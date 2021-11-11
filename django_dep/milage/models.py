554828c1afed19810a64b5e1e7149f1917df1d45
JonR205 <gohan205@gmail.com>
2021-11-10 23:56:14 -0600

    added user egistration page and new post page

--- django_dep/milage/models.py
+++ django_dep/milage/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
@@ -12,5 +12,9 @@ class Post(models.Model):
     date_driven = models.DateField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=CASCADE)
 
+
     def __str__(self):
         return(self.title) 
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
--- django_dep/milage/views.py
+++ django_dep/milage/views.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 from .models import Post
 
 # Create your views here.
@@ -21,6 +26,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content', 'date_driven']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
     
 
 def about(request):
